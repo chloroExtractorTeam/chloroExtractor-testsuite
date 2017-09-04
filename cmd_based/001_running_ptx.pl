12ea5f26dd5b5714c03d8c8fe107b1025c00e605
Frank Förster <foersterfrank@gmx.de>
2017-09-05 00:19:36 +0200

    Running script and testing afterwards

--- cmd_based/001_running_ptx.pl
+++ cmd_based/001_running_ptx.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/env perl
 
+use Test::More tests => 2;
 use Test::Script::Run;
 
-run_ok("ptx", "ptx runs okay");
+my ( $ret, $stdout, $stderr ) = run_script("ptx");
+
+is($ret, 1, 'ptx without argument return with exit code 1');
+ok($stderr =~ /required: --mates.*Usage/sm, 'ptx without argument prints a help message');
